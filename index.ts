f81b0eb9e508d6b42c4a4fb57aa4afb17c6dbf01
LuckShiba <luckshiba@protonmail.com>
2021-02-11 00:50:17 -0300

    added new /file api

--- index.ts
+++ index.ts
@@ -1,13 +1,36 @@
 import IPFS from 'ipfs-core';
-import express from 'express';
+import express, { Response } from 'express';
 import all from 'it-all';
 import mime from 'mime-types';
-import { utimesSync } from 'fs';
 
 IPFS.create().then(async ipfs => {
   const app = express();
-  app.get('/ipfs/*', async (req, res) => {
-    const cid = req.path;
+
+  const sendByCid = async (cid: string, filename: string, res: Response) => {
+    res.set({
+      'Content-Type': mime.lookup(filename) || 'application/octet-stream',
+      'Content-Disposition': `inline; filename="${filename}"`
+    });
+    res.send(Buffer.concat(await all(ipfs.cat(cid))));
+  }
+
+  app.get('/ipfs/file/:cid/:filename', async (req, res) => {
+    try {
+      const { cid, filename } = req.params;
+      await sendByCid(cid, filename, res);
+    }
+    catch(error) {
+      if (error.code === 'ERR_NOT_FOUND') {
+        res.status(404).send('not found');
+      }
+      else {
+        res.status(500).send((error.message));
+      }
+    }
+  })
+
+  app.get('/ipfs/*', async (req, res) => {    
+    const cid = decodeURIComponent(req.path);
     try {
       const items = await all(ipfs.ls(cid));
       if (items.length > 1) {
@@ -17,19 +40,13 @@ IPFS.create().then(async ipfs => {
         }
         res.send(returns);
       } else {
-        const [ item ] = items;
-        const filename = req.query.filename?.toString() || item.name;
-        res.set({
-          'Content-Type': mime.lookup(filename) || 'application/octet-stream',
-          'Content-Disposition': `inline; filename="${filename}"`
-        });
-
-        res.send(Buffer.concat(await all(ipfs.cat(cid))));
+        const filename = req.query.filename?.toString() || items[0].name;
+        await sendByCid(cid, filename, res);
       }
     }
     catch (error) {
       if (error.code === 'ERR_NOT_FOUND') {
-        res.status(404).send('not found')
+        res.status(404).send('not found');
       }
       else {
         res.status(500).send((error.message));
@@ -44,6 +61,6 @@ IPFS.create().then(async ipfs => {
     }
     res.redirect('/ipfs' + req.path + query);
   })
-
-  app.listen(process.env.PORT || 4000, () => console.log('online'));
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`online at port ${PORT}`));
 });
