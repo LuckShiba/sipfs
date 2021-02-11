86ac4dca0eec1a0ecba1b491a91de84db2d48b73
LuckShiba <luckshiba@protonmail.com>
2021-02-11 20:49:29 -0300

    added new /connect api and added cache-control

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import IPFS from 'ipfs-core';
-import express, { Response } from 'express';
+import express, { Response, Request, NextFunction } from 'express';
 import all from 'it-all';
 import mime from 'mime-types';
 
@@ -9,34 +9,41 @@ IPFS.create().then(async ipfs => {
   const sendByCid = async (cid: string, filename: string, res: Response) => {
     res.set({
       'Content-Type': mime.lookup(filename) || 'application/octet-stream',
-      'Content-Disposition': `inline; filename="${filename}"`
+      'Content-Disposition': `inline; filename="${filename}"`,
+      'Cache-Control': 'public, max-age=31536000, immutable, only-if-cached'
     });
     res.send(Buffer.concat(await all(ipfs.cat(cid))));
   }
 
-  app.get('/ipfs/file/:cid/:filename', async (req, res) => {
+  app.get('/ipfs/connect/:path(*)', async (req, res, next) => {
+    try {
+      await ipfs.swarm.connect(IPFS.multiaddr(`/${req.params.path}`));
+      res.status(200).send({ connected: true });
+    }
+    catch (error) {
+      next(error)
+    }
+  });
+
+  app.get('/ipfs/file/:cid/:filename', async (req, res, next) => {
     try {
       const { cid, filename } = req.params;
       await sendByCid(cid, filename, res);
     }
     catch(error) {
-      if (error.code === 'ERR_NOT_FOUND') {
-        res.status(404).send('not found');
-      }
-      else {
-        res.status(500).send((error.message));
-      }
+      next(error);
     }
-  })
+  });
 
-  app.get('/ipfs/*', async (req, res) => {    
-    const cid = decodeURIComponent(req.path);
+  app.get('/ipfs/:path(*)',  async (req, res, next) => {
     try {
+      const cid = decodeURIComponent(req.params.path);
       const items = await all(ipfs.ls(cid));
       if (items.length > 1) {
         const returns: {[keyof: string]: string} = {};
         for (const item of items) {
-          returns[item.name] = item.path;
+          const [path, ...filename] = item.path.split('/');
+          returns[item.name] = `${path}/${filename.map(encodeURIComponent).join('/')}`;
         }
         res.send(returns);
       } else {
@@ -45,12 +52,7 @@ IPFS.create().then(async ipfs => {
       }
     }
     catch (error) {
-      if (error.code === 'ERR_NOT_FOUND') {
-        res.status(404).send('not found');
-      }
-      else {
-        res.status(500).send((error.message));
-      }
+      next(error);
     }
   });
   app.get('/*', (req, res) => {
@@ -61,6 +63,12 @@ IPFS.create().then(async ipfs => {
     }
     res.redirect('/ipfs' + req.path + query);
   })
+
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const status = err.message === 'ERR_NOT_FOUND' ? 404 : 500
+    res.status(status).send(err);
+  });
+
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => console.log(`online at port ${PORT}`));
 });
